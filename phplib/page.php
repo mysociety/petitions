d09db522b48f51980b8d4584aa9f48cd915661ac
francis <francis>
2006-06-15 14:31:28 +0000

    Bit I just missed

---
+++ phplib/page.php
@@ -0,0 +1,74 @@
+<?
+// page.php:
+// Header, footer and other layout parts for pages.
+//
+// Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+// Email: matthew@mysociety.org. WWW: http://www.mysociety.org
+//
+// $Id: page.php,v 1.1 2006-06-15 14:31:28 francis Exp $
+
+require_once '../../phplib/person.php';
+require_once '../../phplib/db.php';
+
+/* page_header TITLE [PARAMS]
+ * Print top part of HTML page, with the given TITLE. This prints up to the
+ * start of the "content" <div>.  If PARAMS['nonav'] is true then the top 
+ * title and navigation are not displayed, or if PARAMS['noprint'] is true
+ * then they are not there if the page is printed. TITLE must be in HTML,
+ * with codes already escape */
+function page_header($title, $params = array()) {
+    // The http-equiv in the HTML below doesn't always seem to override HTTP
+    // header, so we say that we are UTF-8 in the HTTP header as well (Case
+    // where this was required: On my laptop, Apache wasn't setting UTF-8 in
+    // header by default as on live server, and FireFox was defaulting to
+    // latin-1 -- Francis)
+    header('Content-Type: text/html; charset=utf-8');
+
+    $P = person_if_signed_on(true); /* Don't renew any login cookie. */
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title><?
+    if ($title) {
+        print $title . " - ";
+        /* XXX @import url('...') uses single-quotes to hide the style-sheet
+         * from Mac IE. Ugly, but it works. */
+?> Petitions</title>
+</head>
+<body>
+<?
+    }
+
+    // Warn that we are on a testing site
+    $devwarning = array();
+    if (OPTION_PET_STAGING) {
+        $devwarning[] = _('This is a test site for developers only. You probably want
+<a href="http://www.pledgebank.com/">the real site</a>.');
+    }
+    global $pet_today;
+    if ($pet_today != date('Y-m-d')) {
+        $devwarning[] = _("Note: On this test site, the date is faked to be") . " $pet_today";
+    }
+    if (count($devwarning) > 0) {
+        ?><p class="noprint" align="center" style="color: #cc0000; background-color: #ffffff; margin-top: 0;"><?
+        print join('<br>', $devwarning);
+        ?></p><?
+    }
+}
+
+/* page_footer PARAMS
+ * Print bottom of HTML page. This closes the "content" <div>.  
+ * If PARAMS['nonav'] is true then the footer navigation is not displayed. 
+ * If PARAMS['nolocalsignup'] is true then no local signup form is showed.
+ */
+function page_footer($params = array()) {
+?>
+</body></html>
+<?  
+    header('Content-Length: ' . ob_get_length());
+}
+
+
+?>
---
+++ phplib/pet.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * pb.php:
+ * General purpose functions specific to PledgeBank.  This must
+ * be included first by all scripts to enable error logging.
+ * This is only used by the web page PHP scripts, command line ones 
+ * use pbcli.php.
+ * 
+ * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+ * Email: francis@mysociety.org; WWW: http://www.mysociety.org
+ *
+ * $Id: pet.php,v 1.1 2006-06-15 14:31:28 francis Exp $
+ * 
+ */
+
+// Textual content
+$petition_prefix = 'We the undersigned petition the Prime Minister to';
+
+// Load configuration file
+require_once "../conf/general";
+// Some early config files - put most config files after language negotiation below
+require_once "../../phplib/error.php";
+require_once 'page.php';
+
+/* Output buffering: PHP's output buffering is broken, because it does not
+ * affect headers. However, it's worth using it anyway, because in the common
+ * case of outputting an HTML page, it allows us to clear any output and
+ * display a clean error page when something goes wrong. Obviously if we're
+ * displaying an error, a redirect, an image or anything else this will break
+ * horribly.*/
+ob_start();
+
+/* pet_handle_error NUMBER MESSAGE
+ * Display a PHP error message to the user. */
+function pet_handle_error($num, $message, $file, $line, $context) {
+    if (OPTION_PET_STAGING) {
+        while (ob_get_level()) {
+            ob_end_clean();
+        }
+        ob_start(); // since page header writes content length, must be in ob_
+        page_header(_("Sorry! Something's gone wrong."), array('override'=>true));
+        print("<strong>$message</strong> in $file:$line");
+        page_footer();
+    } else {
+        /* Nuke any existing page output to display the error message. */
+        while (ob_get_level()) {
+            ob_end_clean();
+        }
+        /* Message will be in log file, don't display it for cleanliness */
+        $err = p(_('Please try again later, or XXXemail usXXX for help resolving the problem.'));
+        if ($num & E_USER_ERROR) {
+            $err = "<p><em>$message</em></p> $err";
+        }
+        ob_start(); // since page header writes content length, must be in ob_
+        pet_show_error($err);
+    }
+}
+err_set_handler_display('pet_handle_error');
+
+/* Date which petition application believes it is */
+$pet_today = db_getOne('select pet_current_date()');
+$pet_timestamp = substr(db_getOne('select pet_current_timestamp()'), 0, 19);
+$pet_time = strtotime($pet_timestamp);
+
+/* pet_show_error MESSAGE
+ * General purpose eror display. */
+function pet_show_error($message) {
+    header('HTTP/1.0 500 Internal Server Error');
+    page_header(_("Sorry! Something's gone wrong."), array('override'=>true));
+    print _('<h2>Sorry!  Something\'s gone wrong.</h2>') .
+        "\n<p>" . $message . '</p>';
+    page_footer();
+}
+
+
