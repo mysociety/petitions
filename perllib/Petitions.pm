26d0dd0651368d496993819d28010e127eaeac15
chris <chris>
2006-07-18 16:48:13 +0000

    More boilerplate.

--- perllib/Petitions.pm
+++ perllib/Petitions.pm
@@ -6,16 +6,20 @@
 # Copyright (c) 2006 Chris Lightfoot. All rights reserved.
 # Email: chris@ex-parrot.com; WWW: http://www.ex-parrot.com/~chris/
 #
-# $Id: Petitions.pm,v 1.1 2006-07-10 13:48:47 chris Exp $
+# $Id: Petitions.pm,v 1.2 2006-07-18 16:48:13 chris Exp $
 #
 
 package Petitions::DB;
 
 use strict;
 
+use DBI;
+
 use mySociety::Config;
 use mySociety::DBHandle qw(dbh);
-use DBI;
+use mySociety::Util qw(random_bytes print_log is_valid_email);
+
+my $secret;
 
 BEGIN {
     mySociety::DBHandle::configure(
@@ -23,9 +27,24 @@ BEGIN {
             User => mySociety::Config::get('PET_DB_USER'),
             Password => mySociety::Config::get('PET_DB_PASS'),
             Host => mySociety::Config::get('PET_DB_HOST', undef),
-            Port => mySociety::Config::get('PET_DB_PORT', undef)
+            Port => mySociety::Config::get('PET_DB_PORT', undef),
+            OnFirstUse => sub {
+                if (!dbh()->selectrow_array('select secret from secret')) {
+                    local dbh()->{HandleError};
+                    dbh()->do('insert into secret (secret) values (?)',
+                                {}, unpack('h*', random_bytes(32)));
+                    dbh()->commit();
+                }
         );
-    # probably don't need a secret here
+}
+
+=item secret
+
+Return the site shared secret.
+
+=cut
+sub secret () {
+    return scalar(dbh()->selectrow_array('select secret from secret'));
 }
 
 package Petitions;
