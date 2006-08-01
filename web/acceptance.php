a544ebcb6fa55cbf8153fbcc835ea364c223ed57
chris <chris>
2006-08-01 21:45:47 +0000

    XHTML. Minor formatting change. Consistent punctuation of bullet points.

--- web/acceptance.php
+++ web/acceptance.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: acceptance.php,v 1.1 2006-07-13 15:46:53 matthew Exp $
+// $Id: acceptance.php,v 1.2 2006-08-01 21:45:47 chris Exp $
 
 // Load configuration file
 require_once "../phplib/pet.php";
@@ -20,31 +20,31 @@ of the following criteria:</p>
 
 <ul>
 
-<li>Party political material. The Downing Street website is a
-Government site. Party political content is not appropriate, under the
-normal rules governing the Civil Service;
+<li>Party political material&mdash;the Downing Street website is a
+Government site, so party political content is not appropriate under the
+normal rules governing the Civil Service;</li>
 
-<li>false or defamatory statements;
+<li>false or defamatory statements;</li>
 
 <li>information protected by an injunction or court order (for
-example, the identities of children in custody disputes);
+example, the identities of children in custody disputes);</li>
 
 <li>material which is commercially sensitive, confidential or which
-may cause personal distress or loss;
+may cause personal distress or loss;</li>
 
 <li>the names of individual officials of public bodies, unless part
-of the senior management of those organisations.
+of the senior management of those organisations;</li>
 
 <li>the names of family members of officials of public bodies, or
-elected representatives.,
+elected representatives;</li>
 
 <li>the names of individuals, or information where they may be
-identified, in relation to criminal accusations;
+identified, in relation to criminal accusations;</li>
 
 <li>offensive language, such as obvious swear words or language that
 is intemperate, inflammatory, or provocative, or to which people
-reading it could reasonably take offence.
+reading it could reasonably take offence.</li>
 </ul>
 
-<?  page_footer();
-
+<?
+page_footer();
