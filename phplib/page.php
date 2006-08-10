9707ea118f0cad94c1d1a01098f849573c8bc023
chris <chris>
2006-08-10 13:53:52 +0000

    Remove references to person_ functions.

--- phplib/page.php
+++ phplib/page.php
@@ -5,9 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: page.php,v 1.9 2006-08-10 12:39:23 matthew Exp $
-
-require_once '../../phplib/person.php';
+// $Id: page.php,v 1.10 2006-08-10 13:53:52 chris Exp $
 
 /* page_header TITLE [PARAMS]
  * Print top part of HTML page, with the given TITLE. This prints up to the
@@ -22,9 +20,6 @@ function page_header($title, $params = array()) {
     // header by default as on live server, and FireFox was defaulting to
     // latin-1 -- Francis)
     header('Content-Type: text/html; charset=utf-8');
-
-    # XXX: Are we going to show people they are logged in?
-    $P = person_if_signed_on(true); /* Don't renew any login cookie. */
 
     // Warn that we are on a testing site
     global $devwarning;
