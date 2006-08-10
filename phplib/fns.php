6d56c4b84b8a46d8869b127305a8f3ccd7f44821
chris <chris>
2006-08-10 17:28:07 +0000

    Function for sending petition messages to admin, creator or signers.

--- phplib/fns.php
+++ phplib/fns.php
@@ -5,10 +5,63 @@
 // Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
 // Email: francis@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: fns.php,v 1.2 2006-06-27 22:40:28 matthew Exp $
+// $Id: fns.php,v 1.3 2006-08-10 17:28:07 chris Exp $
 
 require_once "../../phplib/evel.php";
 require_once '../../phplib/utility.php';
+
+define(MSG_ADMIN, 1);
+define(MSG_CREATOR, 2);
+define(MSG_SIGNERS, 4);
+define(MSG_ALL, MSG_ADMIN | MSG_CREATOR | MSG_SIGNERS);
+
+/* pet_send_message ID SENDER RECIPIENTS CIRCUMSTANCE TEMPLATE
+ * Send a message to the RECIPIENTS in respect of the petition with the given
+ * ID, constructing it from the given email TEMPLATE. RECIPIENTS should be the
+ * bitwise combination of one or more of MSG_ADMIN, MSG_CREATOR and
+ * MSG_SIGNERS. The message will appear to come from SENDER, which must be
+ * MSG_ADMIN or MSG_CREATOR; CIRCUMSTANCE indicates the reason for its
+ * sending. */
+function pet_send_message($petition_id, $sender, $recips, $template) {
+    if(!is_int($petition_id))
+        err("ID must be integer in pet_send_message");
+
+    if ($sender != MSG_ADMIN && $sender != MSG_CREATOR)
+        err("SENDER must be MSG_ADMIN or MSG_CREATOR");
+
+    if ($recips == 0)
+        err("RECIPIENTS must be nonzero in pet_send_message");
+    elseif ($id & ~MSG_ALL)
+        err("Unknown bits present in RECIPIENTS $id");
+
+    db_query("
+            insert into message (
+                petition_id,
+                circumstance,
+                circumstance_count,
+                fromaddress,
+                sendtoadmin, sendtocreator, sendtosigners, sendtolatesigners,
+                emailtemplatename
+            ) values (
+                ?,
+                ?,
+                coalesce((select max(circumstance_count)
+                            from message where petition_id = ?
+                                and circumstance = ?), 0) + 1,
+                ?,
+                ?, ?, ?, 'f', -- XXX
+                ?
+            )",
+            $petition_id,
+            $circumstance,
+                $petition_id,
+                $circumstance,
+            $sender == MSG_ADMIN ? 'number10' : 'creator',
+                ($recips & MSG_ADMIN) ? 't' : 'f',
+                ($recips & MSG_CREATOR) ? 't' : 'f',
+                ($recips & MSG_SIGNERS) ? 't' : 'f',
+            $template);
+}
 
 // $to can be one recipient address in a string, or an array of addresses
 function pet_send_email_template($to, $template_name, $values, $headers = array()) {
