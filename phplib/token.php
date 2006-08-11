6d413230e442a62334684f090a0770afc7865774
chris <chris>
2006-08-11 18:09:58 +0000

    Blowfish not IDEA. Cope with stupid definition of the unpack function (for some reason list($fish) = unpack('C0', ...) doesn't seem to work).

--- phplib/token.php
+++ phplib/token.php
@@ -6,21 +6,22 @@
  * Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: token.php,v 1.1 2006-08-11 16:07:09 chris Exp $
+ * $Id: token.php,v 1.2 2006-08-11 18:09:58 chris Exp $
  * 
  */
 
-require 'Crypt_HMAC.php';
+require_once '../../phplib/db.php';
+require_once '../../phplib/random.php';
 
 function token_encode_base64ish($in) {
     return
         preg_replace('/\+/', '_',
             preg_replace('/\//', '-',
-                preg_replace('/=+$/', '',
+                preg_replace('/=+\n/s', '',
                     base64_encode($in))));
 }
 
-function token_decode_bnase64ish($in) {
+function token_decode_base64ish($in) {
     while (strlen($in) % 4)
         $in .= '=';
     return
@@ -41,22 +42,24 @@ function token_make($what, $id) {
         err("ID must be a positive integer");
 
     /* see explanation of format in Petitions.pm */
-    $salt = random_bytes(3);
     $s1 = unpack('C', random_bytes(1));
+    $s1 = $s1[1];
     $s1 &= 0x3f;
     if ($what == 's')
         $s1 |= 0x40;
     elseif ($what == 'e')
         $s1 |= 0x80;
 
-    $plaintext = pack('C', $s1) . $salt . pack('N', $id);
+    $plaintext = pack('C', $s1) . random_bytes(3) . pack('N', $id);
+
     $hmac = mhash(MHASH_SHA1, $plaintext, db_secret());
 
     $ciphertext = mcrypt_encrypt(
-                        MCRYPT_IDEA,
-                        substr(mhash(MHASH_SHA1, db_secret()), 0,
-                            mcrypt_enc_get_block_size(MCRYPT_IDEA)),
-                        MCRYPT_MODE_ECB);
+                        MCRYPT_BLOWFISH,
+                        substr(mhash(MHASH_SHA1, db_secret()), 0, 8),
+                        $plaintext,
+                        MCRYPT_MODE_ECB,
+                        "\0\0\0\0\0\0\0\0");
 
     return token_encode_base64ish($ciphertext . substr($hmac, 0, 7));
 }
@@ -72,16 +75,20 @@ function token_check($token) {
     $ciphertext = substr($data, 0, 8);
     $hmac7 = substr($data, 8, 7);
 
-    $plaintext = mcrypt_decrypt(MCRYPT_IDEA,
-                    substr(mhash(MHASH_SHA1, db_secret()), 0,
-                        mcrypt_enc_get_block_size(MCRYPT_IDEA)),
-                    MCRYPT_MODE_ECB);
+    $plaintext = mcrypt_decrypt(
+                    MCRYPT_BLOWFISH,
+                    substr(mhash(MHASH_SHA1, db_secret()), 0, 8),
+                    $ciphertext,
+                    MCRYPT_MODE_ECB,
+                    "\0\0\0\0\0\0\0\0");
 
     $hmac = mhash(MHASH_SHA1, $plaintext, db_secret());
     if (substr($hmac, 0, 7) != $hmac7)
         return array();
 
     $s1 = unpack('C', $plaintext);
+    $s1 = $s1[1];
+
     if (($s1 & 0xc0) == 0xc0)   /* reserved for future expansion */
         return array();
 
@@ -93,6 +100,7 @@ function token_check($token) {
         $what = 'p';
 
     $id = unpack('N', substr($plaintext, 4, 4));
+    $id = $id[1];
 
     return array($what, $id);
 }
