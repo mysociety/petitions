09e59ae4deab34ca7f5f2ed0574ba902c051a28c
matthew <matthew>
2006-11-16 12:05:35 +0000

    encode_base64() sticks a \n on the end by default, lovely.

--- perllib/Petitions.pm
+++ perllib/Petitions.pm
@@ -6,7 +6,7 @@
 # Copyright (c) 2006 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: Petitions.pm,v 1.31 2006-11-16 11:34:18 matthew Exp $
+# $Id: Petitions.pm,v 1.32 2006-11-16 12:05:35 matthew Exp $
 #
 
 package Petitions::DB;
@@ -143,7 +143,7 @@ use mySociety::DBHandle ();
 use mySociety::Util qw(random_bytes);
 
 sub encode_base64ish ($) {
-    my $b64 = encode_base64($_[0]);
+    my $b64 = encode_base64($_[0], '');
     $b64 =~ s#\+#_#g;
     $b64 =~ s#/#-#g;
     $b64 =~ s#=+$##;
@@ -155,7 +155,7 @@ sub decode_base64ish ($) {
     while (length($b64) % 4) {
         $b64 .= '=';
     }
-    $b64 =~ s#_#+#g;
+    $b64 =~ s#\$|_#+#g;
     $b64 =~ s#-#/#g;
     return decode_base64($b64);
 }
