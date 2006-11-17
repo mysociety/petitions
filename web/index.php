d30a6b1b7b2c4d7596f3be960f52555caf3e6118
francis <francis>
2006-11-17 19:19:52 +0000

    Make search box always on one line, rather than wrapping to the right of Create/View petitions box on some font sizes.  CVS: ----------------------------------------------------------------------

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: index.php,v 1.31 2006-11-17 17:10:39 matthew Exp $
+// $Id: index.php,v 1.32 2006-11-17 19:19:52 francis Exp $
 
 // Load configuration file
 require_once "../phplib/pet.php";
@@ -36,7 +36,7 @@ the Number 10 door in person. You can now both create and sign petitions on
 this website too, giving you the opportunity to reach a potentially wider audience
 and to deliver your petition directly to Downing Street.</p>
 
-<form name="kbs" method="get" action="/search">
+<form name="kbs" method="get" action="/search" style="clear:both">
 <p><br /><label for="q"><strong>Search petitions:</strong></label>
 <input type="text" name="q" id="q" maxlength="1000" value="" />&nbsp;<input type="submit" value="Go" /></p>
 </form>
