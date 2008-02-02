d6ff16c010015c71b6aafea7fb247761eed40b43
matthew <matthew>
2008-02-02 19:45:53 +0000

    And some more.

--- perllib/Petitions/Page.pm
+++ perllib/Petitions/Page.pm
@@ -6,7 +6,7 @@
 # Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 # Email: chris@mysociety.org; WWW: http://www.mysociety.org/
 #
-# $Id: Page.pm,v 1.96 2008-02-02 00:01:46 matthew Exp $
+# $Id: Page.pm,v 1.97 2008-02-02 19:45:54 matthew Exp $
 #
 
 package Petitions::Page;
@@ -31,10 +31,10 @@ use Petitions;
 my $exit_requested = 0;
 my $handling_request = 0;
 
-$SIG{TERM} = $SIG{USR1} = sub {
-    $exit_requested = 1;
-    # exit(0) unless $handling_request;
-};
+#$SIG{TERM} = $SIG{USR1} = sub {
+#    $exit_requested = 1;
+#    # exit(0) unless $handling_request;
+#};
 
 sub do_fastcgi {
     my $func = shift;
