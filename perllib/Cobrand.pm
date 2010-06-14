dc3dc7aecafd72d705009cf9955e1e37e62e6c96
Matthew Somerville <matthew@balti.ukcod.org.uk>
2010-06-14 23:59:01 +0100

    Include new file.

---
+++ perllib/Cobrand.pm
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+#
+# Cobrand.pm:
+# Petition cobrand utilities.
+#
+# Copyright (c) 2010 UK Citizens Online Democracy. All rights reserved.
+# Email: matthew@mysociety.org; WWW: http://www.mysociety.org/
+#
+
+package Cobrand;
+
+use strict;
+use Petitions::Page;
+
+sub main_heading($) {
+    my $text = shift;
+    my $site_name = Petitions::Page::site_name();
+    return "<h2>$text</h2>" if $site_name == 'surreycc';
+    return "<h3 class='page_title_border'>$text</h3>" if $site_name == 'number10';
+    return "<h3>$text</h3>";
+}
+
+1;
