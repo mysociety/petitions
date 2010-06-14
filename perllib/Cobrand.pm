88f4f7503f64d285a752b34a70d849aaaff910de
Matthew Somerville <matthew@balti.ukcod.org.uk>
2010-06-15 00:00:25 +0100

    Wrong ==/eq

--- perllib/Cobrand.pm
+++ perllib/Cobrand.pm
@@ -15,8 +15,8 @@ use Petitions::Page;
 sub main_heading($) {
     my $text = shift;
     my $site_name = Petitions::Page::site_name();
-    return "<h2>$text</h2>" if $site_name == 'surreycc';
-    return "<h3 class='page_title_border'>$text</h3>" if $site_name == 'number10';
+    return "<h2>$text</h2>" if $site_name eq 'surreycc';
+    return "<h3 class='page_title_border'>$text</h3>" if $site_name eq 'number10';
     return "<h3>$text</h3>";
 }
 
