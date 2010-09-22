f4035887ac045cc5d10f12b64bd86ce96ab300cd
Matthew Somerville <matthew@balti.ukcod.org.uk>
2010-09-22 13:47:23 +0100

    Mole Valley style tweaks.

--- phplib/cobrand.php
+++ phplib/cobrand.php
@@ -719,7 +719,7 @@ function cobrand_create_heading($text) {
 # main heading that one council asked for.
 function cobrand_extra_heading($text) {
     global $site_name;
-    if ($site_name == 'tandridge')
+    if ($site_name == 'tandridge' || $site_name == 'molevalley')
         print "<h1>$text</h1>";
 }
 
