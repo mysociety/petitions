ead465c70d5fee7351232b999f0be1192d569aa7
Dave Whiteland <dave@balti.ukcod.org.uk>
2010-11-26 10:18:14 +0000

    indented email-won't-be-published note on signup for Westminster

--- perllib/Petitions/Page.pm
+++ perllib/Petitions/Page.pm
@@ -374,7 +374,7 @@ the Armed Forces without a postcode, please select from this list:</label>',
                 $q->textfield(-name => 'email', -size => 20, -id => 'email', -aria_required => 'true'))
         . $q->p( '<label for="email2">Confirm email:</label>',
                 $q->textfield(-name => 'email2', -size => 20, -id => 'email2', -aria_required => 'true', -autocomplete => 'off'))
-        . $q->p($q->strong('Your email will not be published,'), 'and is collected only to confirm your account and to keep you informed of response to this petition.')
+        . $q->p({ -id => 'ms-email2-note-signup'}, $q->strong('Your email will not be published,'), 'and is collected only to confirm your account and to keep you informed of response to this petition.')
         )
         . $q->div({-id => 'signFormRight' },
           $address,
