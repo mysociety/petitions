9151383f8f6dff199385cfb6d6e1095ce64761e2
Dave Whiteland <dave@balti.ukcod.org.uk>
2010-12-06 13:08:57 +0000

    Added 10+1 day rejection timeout for Rushcliffe

--- phplib/cobrand.php
+++ phplib/cobrand.php
@@ -1045,6 +1045,7 @@ function cobrand_allowed_responses() {
 function cobrand_rejected_petition_timeout() {
     global $site_group; # No $site_name available
     if ($site_group == 'westminster') return '8 days';
+    if ($site_group == 'nottinghamshire') return array('rushcliffe' => '11 days', 'other' => '29 days');
     if ($site_group == 'surreycc') return array('elmbridge' => '15 days', 'other' => '29 days');
     // 29 days is 4 weeks, plus a day to allow a margin for the creator
     return '29 days';
