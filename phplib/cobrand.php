569369be60991db97595d4223184880deb963ad9
Matthew Somerville <matthew@mysociety.org>
2011-01-11 14:38:18 +0000

    Salford branding, set up.

--- phplib/cobrand.php
+++ phplib/cobrand.php
@@ -159,6 +159,7 @@ function cobrand_creation_within_area_only() {
     if ($site_name == 'runnymede') return array('Runnymede', 2451);
     if ($site_name == 'rushcliffe') return array('Rushcliffe', 2411);
     if ($site_name == 'rbwm') return array('the Royal Borough of Windsor and Maidenhead', 2622);
+    if ($site_name == 'salford') return array('Salford', 2534);
     if ($site_name == 'sholland') return array('South Holland', 2381);
     if ($site_name == 'spelthorne') return array('Spelthorne', 2456);
     if ($site_name == 'stedmundsbury') return array('the borough of St Edmundsbury', 2443);
@@ -409,16 +410,12 @@ petition in this category</a>.";
         }
         if ($area) {
             # $area is set if we're being called as a result of the form below
-            if (in_array($area, array('tandridge', 'reigate-banstead', 'woking', 'spelthorne', 'runnymede', 'elmbridge', 'waverley')))
+            if (in_array($area, array('tandridge', 'reigate-banstead', 'woking', 'spelthorne', 'runnymede', 'elmbridge', 'waverley', 'surreyheath', 'molevalley')))
                 return 'http://petitions.' . $area . '.gov.uk/new?tostepmain=1&category=' . $category_id;
             if ($area == 'epsom-ewell')
                 return 'http://www.epsom-ewell.gov.uk/EEBC/Council/E-petitions.htm';
             if ($area == 'guildford')
                 return 'http://www.surreycc.gov.uk/SCCWebsite/SCCWSPages.nsf/LookupWebPagesByUNID_RTF_INT/A4F9AD1334EF7EB480257744005476BA?opendocument';
-            if ($area == 'molevalley')
-                return 'http://www.molevalley.gov.uk/index.cfm?articleid=9694';
-            if ($area == 'surreyheath')
-                return 'http://www.surreyheath.gov.uk/council/epetitions/';
         } else {
             return '
             <input type="hidden" name="category" value="' . $category_id . '"> 
