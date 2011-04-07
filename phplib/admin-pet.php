309f80847f91fca9865f8f066dd5e4c1d77e896e
Matthew Somerville <matthew@fury.ukcod.org.uk>
2011-04-07 15:38:03 +0100

    Pretty display of month.

--- phplib/admin-pet.php
+++ phplib/admin-pet.php
@@ -459,7 +459,6 @@ class ADMIN_PAGE_PET_MAIN {
                 (ms_current_timestamp() - interval '7 days' > laststatuschange) AS late, 
                 (deadline + interval '1 year' >= ms_current_date()) AS response_possible,
                 cached_signers AS signers,
-                date_trunc('month', archived) as archived_month,
                 $surge
                 message.c AS message_count
             FROM petition
@@ -541,7 +540,7 @@ class ADMIN_PAGE_PET_MAIN {
                 $row .= '</td>';
             } elseif ($status == 'archived') {
                 $row .= '<td>';
-                $row .= $r['archived_month'];
+                $row .= date('F Y', strtotime($r['archived']));
                 $row .= '</td>';
             }
             $found[] = array($late, $row);
