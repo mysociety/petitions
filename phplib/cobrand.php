eea799e387fcb48f5b8c13451d8842631af1d54c
Steven Day <steve@mysociety.org>
2016-08-12 09:19:21 +0100

    Add a 6 week deadline limit for Westminster

--- phplib/cobrand.php
+++ phplib/cobrand.php
@@ -97,8 +97,10 @@ function cobrand_creation_deadline_limit($body='') {
         return array('years' => 0, 'months' => 6);
     if ($body == 'wellingborough')
         return array('years' => 0, 'months' => 4);
-    if ($body == 'eastcambs' || $body == 'stedmundsbury' || $body == 'westminster')
+    if ($body == 'eastcambs' || $body == 'stedmundsbury')
         return array('years' => 0, 'months' => 3);
+    if ($body == 'westminster')
+        return array('weeks' => 6);
     if ($body == 'barrowbc' || $body == 'bassetlaw')
         return array('date' => '2011-12-01');
     if ($body == 'mansfield')
